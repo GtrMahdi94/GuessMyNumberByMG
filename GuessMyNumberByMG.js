6bdab842cd119e06c043bb724252adc1a418a31b
Gatri Mahdi <gatrimahdi5@gmail.com>
2023-12-24 13:21:42 +0100

    first code done

---
+++ GuessMyNumberByMG.js
@@ -0,0 +1,37 @@
+var x= Math.floor(Math.random() * 101)
+ var z=1
+function guessMyNumberByMG (number){
+   
+    
+    if (z===1){
+    console.log ('here we began your first try ')
+        z=z+1;
+    }
+    else if (z<6) {
+      
+      console.log (z+' Try ')
+        z=z+1; 
+        
+    }
+    else{
+        return ("you lose")
+    
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    }
