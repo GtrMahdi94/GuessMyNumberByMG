e2a380f7aee4bf1c58c683bf4a602b0541cb7a36
Gatri Mahdi <gatrimahdi5@gmail.com>
2023-12-24 13:38:39 +0100

    v0.1

--- GuessMyNumberByMG.js
+++ GuessMyNumberByMG.js
@@ -1,34 +1,33 @@
 var x= Math.floor(Math.random() * 101)
- var z=1
+ var z=0
 function guessMyNumberByMG (number){
    
     
-    if (z===1){
+    if (z===0){
     console.log ('here we began your first try ')
         z=z+1;
     }
-    else if (z<6) {
-      
+    else if (z<5) {
+       z=z+1
       console.log (z+' Try ')
-        z=z+1; 
+       ; 
         
     }
     else{
-        return ("you lose")
+        return ("You Already Lose")
     
     }
-    
 
-
-
-
-
-
-
-
-
-
-
+    if (number===x){
+        return ("You Did it in "+z+" Times")
+    }
+       else if (x<number){
+        console.log("Try a Lower Number ")
+           
+       }
+    else if (x>number){
+        console.log ("Try a Higher Number ")
+    }
 
 
 
